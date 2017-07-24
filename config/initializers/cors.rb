9c32c213dc2ad28f1b9bb18083e0fa1ec37a24e1
Chisomo Lungu <chisomo@gmail.com>
2017-07-24 06:52:37 +0100

    added heroku site to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:7000', 'https://intense-oasis-59348.herokuapp.com', 'https://boiling-reaches-88375.herokuapp.com', 'https://boiling-reaches-88375.herokuapp.com/api/oauth/facebook']
-
+    # origins ['http://localhost:7000', 'https://intense-oasis-59348.herokuapp.com', 'https://boiling-reaches-88375.herokuapp.com', 'https://boiling-reaches-88375.herokuapp.com/api/oauth/facebook']
+    origins ['*']
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
