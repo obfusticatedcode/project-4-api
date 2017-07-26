b52de975fdc7803b1caf301fc77d69651d71c025
Chisomo Lungu <chisomo@gmail.com>
2017-07-26 03:04:02 +0100

    Heroku Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins ['http://localhost:7000', 'https://intense-oasis-59348.herokuapp.com', 'https://boiling-reaches-88375.herokuapp.com']
-    origins ['*']
+    origins ['http://localhost:7000', 'https://intense-oasis-59348.herokuapp.com', 'https://boiling-reaches-88375.herokuapp.com']
 
     resource '*',
       headers: :any,
